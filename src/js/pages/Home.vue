dce0cd5d352f3e007e49959716d8cc0bb08d939b
Alexandra Moore <alexmoore94@hotmail.co.uk>
2017-09-25 20:20:49 +0100

    browserify and js structure set up

---
+++ src/js/pages/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <h1>Hello</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+}
+</style>
