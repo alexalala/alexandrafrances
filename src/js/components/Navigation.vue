5b467663e8c263d1677c53da5ef0881cd5b2df10
Alexandra Moore <alexmoore94@hotmail.co.uk>
2017-10-01 21:20:25 +0100

    nav styled

--- src/js/components/Navigation.vue
+++ src/js/components/Navigation.vue
@@ -12,3 +12,15 @@ export default {
   name: 'Navigation'
 }
 </script>
+
+<style lang="sass">
+  ul {
+    list-style: none;
+    text-align: right;
+    margin: 1rem 1rem;
+    li {
+      display: inline-block;
+      margin: 0 1rem;
+    }
+  }
+</style>
