574be33ad315eb0f4e5d8f35453c1171971259a1
Alexandra Moore <alexmoore94@hotmail.co.uk>
2017-10-17 15:22:18 +0100

    vou router set up and needless app component removed

--- src/js/pages/Home.vue
+++ src/js/pages/Home.vue
@@ -1,17 +1,48 @@
 <template>
-  <div id="app">
-    <navigation></navigation>
-    <main-content></main-content>
-    <footr></footr>
+  <div class="content">
+    <div class="container">
+      <cloud></cloud>
+      <cloud2></cloud2>
+      <cloud3></cloud3>
+    </div>
+    <building1></building1>
+    <road></road>
   </div>
 </template>
 
 <script>
-import Navigation from '../components/Navigation.vue';
-import MainContent from '../components/MainContent.vue';
-import Footr from '../components/Footr.vue';
+import Cloud from '../components/clouds/Cloud.vue';
+import Cloud2 from '../components/clouds/Cloud2.vue';
+import Cloud3 from '../components/clouds/Cloud3.vue';
+import Building1 from '../components/buildings/Building1.vue';
+import Road from '../components/misc/Road.vue';
 
 export default {
-  name: 'App'
-};
+  name: 'MainContent'
+}
 </script>
+
+<style lang="sass">
+  @import "src/styles/components/vars.scss";
+
+  body {
+    padding: 0;
+    overflow: hidden;
+    margin: 0;
+
+    .content {
+      background-color: $blue;
+      height: 85vh;
+      position: relative;
+
+      .container {
+        width: 100%;
+        position: relative;
+        height: 400px;
+        overflow: hidden;
+        margin: auto;
+        margin: 0;
+      }
+    }
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue';
-import App from './js/pages/Home.vue';
+import VueRouter from 'vue-router';
+
+import Home from './js/pages/Home.vue';
+
 import Navigation from './js/components/Navigation.vue';
-import MainContent from './js/components/MainContent.vue';
 import Footr from './js/components/Footr.vue';
+
 import Cloud from './js/components/clouds/Cloud.vue';
 import Cloud2 from './js/components/clouds/Cloud2.vue';
 import Cloud3 from './js/components/clouds/Cloud3.vue';
@@ -10,7 +13,6 @@ import Building1 from './js/components/buildings/Building1.vue';
 import Road from './js/components/misc/Road.vue';
 
 Vue.component('navigation', Navigation);
-Vue.component('main-content', MainContent);
 Vue.component('footr', Footr);
 Vue.component('cloud', Cloud);
 Vue.component('cloud2', Cloud2);
@@ -18,7 +20,15 @@ Vue.component('cloud3', Cloud3);
 Vue.component('building1', Building1);
 Vue.component('road', Road);
 
+Vue.use(VueRouter);
+
+const router = new VueRouter ({
+  routes: [
+    { path: '/', name: 'Home', component: Home }
+  ]
+});
+
 new Vue({
   el: '#app',
-  render: h => h(App)
+  router: router
 });
