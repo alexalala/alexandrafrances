4a0b99ff66881e708fb8ca1f60dcd7f63c86aa32
Alexandra Moore <alexmoore94@hotmail.co.uk>
2017-10-28 10:22:50 +0100

    windows added and building styled

--- src/js/components/buildings/OfficeBuilding.vue
+++ src/js/components/buildings/OfficeBuilding.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="offices">
+    <div class="windows"></div>
   </div>
 </template>
 
@@ -9,5 +10,29 @@
   };
 </script>
 
-<style>
+<style lang="sass">
+  @import "src/styles/components/vars.scss";
+
+  .offices {
+    background-color: $dark-grey;
+    height: 45vh;
+    bottom: 0;
+    position: absolute;
+    width: 15vw;
+    margin-left: 18vw;
+    border-radius: 5px;
+
+    .windows {
+      height: 90%;
+      width: 80%;
+      margin-left: 10%;
+      background: repeating-linear-gradient(
+        to bottom,
+        $dark-grey,
+        $dark-grey 20px,
+        $white 20px,
+        $white 40px
+      );
+    }
+  }
 </style>
