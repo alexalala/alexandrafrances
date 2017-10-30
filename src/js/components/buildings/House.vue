f1ebb0981d6326e46efc34a1132516235cb18597
Alexandra Moore <alexmoore94@hotmail.co.uk>
2017-10-30 18:12:40 +0000

    house styled

--- src/js/components/buildings/House.vue
+++ src/js/components/buildings/House.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="house">
     <div class="roof"></div>
-    <div class="walls"></div>
+    <div class="walls">
+      <div class="window"></div>
+      <div class="door">
+        <div class="doorknob"></div>
+      </div>
+      <div class="window"></div>
+    </div>
   </div>
 </template>
 
@@ -25,9 +31,46 @@
     .roof {
       width: 0;
       height: 0;
-      border-left: 100px solid transparent;
-      border-right: 100px solid transparent;
-      border-bottom: 100px solid $pink;
+      border-left: 110px solid transparent;
+      border-right: 110px solid transparent;
+      border-bottom: 100px solid $brick-red;
+      margin-left: -10px;
+      position: relative;
+    }
+    .walls {
+      height: 100px;
+      width: 200px;
+      background-color: $red;
+      position: relative;
+
+      .window {
+        height: 40px;
+        width: 40px;
+        background-color: $grey;
+        position: absolute;
+      }
+      .window:nth-child(1) {
+        margin: 20px;
+      }
+      .windw:nath-child(2) {
+        margin: 20px 0 0 140px;
+      }
+      .door {
+        height: 50px;
+        width: 30px;
+        background-color: $green;
+        margin-left: 85px;
+        position: absolute;
+        bottom: 0;
+
+        .doorknob {
+          height: 5px;
+          width: 5px;
+          border-radius: 4px;
+          background-color: $gold;
+          margin: 20px;
+        }
+      }
     }
   }
 </style>
