bb991af7708c54ee0df865a1667ca68bdcaa587b
Alexandra Moore <alexmoore94@hotmail.co.uk>
2017-10-30 12:08:15 +0000

    roof added

--- src/js/components/buildings/House.vue
+++ src/js/components/buildings/House.vue
@@ -1,5 +1,8 @@
 <template>
-  <p> Hello </p>
+  <div class="house">
+    <div class="roof"></div>
+    <div class="walls"></div>
+  </div>
 </template>
 
 <script>
@@ -9,4 +12,22 @@
 </script>
 
 <style lang="sass">
+
+  @import "src/styles/components/vars.scss";
+
+  .house {
+    height: 200px;
+    width: 200px;
+    margin-left: 32vw;
+    bottom: 17rem;
+
+
+    .roof {
+      width: 0;
+      height: 0;
+      border-left: 100px solid transparent;
+      border-right: 100px solid transparent;
+      border-bottom: 100px solid $pink;
+    }
+  }
 </style>
